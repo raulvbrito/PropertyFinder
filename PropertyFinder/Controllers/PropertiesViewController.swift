2b680a6ee9414a664ae22b0a0a1e5f651518d501
Raul Brito <raulvbrito@gmail.com>
2019-02-21 22:21:09 -0300

    SearchController for Properties TableView

--- PropertyFinder/Controllers/PropertiesViewController.swift
+++ PropertyFinder/Controllers/PropertiesViewController.swift
@@ -12,7 +12,10 @@ class PropertiesViewController: BaseViewController {
 	
 	// MARK: - Properties
 	
+	let searchController = UISearchController(searchResultsController: nil)
+	
 	var propertyViewModels = [PropertyViewModel]()
+	var filteredPropertyViewModels = [PropertyViewModel]()
 	
 	@IBOutlet var tableView: UITableView!
 	
@@ -23,12 +26,13 @@ class PropertiesViewController: BaseViewController {
         super.viewDidLoad()
 		
 		fetchData()
+		searchSetup()
     }
 	
 	
 	// MARK: - Methods
 	
-    func fetchData() {
+	@objc func fetchData() {
 		PropertyService.fecthProperties { (error, properties) in
 			if let error = error {
 				print(error.domain)
@@ -36,7 +40,35 @@ class PropertiesViewController: BaseViewController {
 			}
 			
 			self.propertyViewModels = properties.map({return PropertyViewModel(property: $0)})
+			self.tableView.reloadData()
 		}
+	}
+	
+	func searchSetup() {
+		searchController.searchResultsUpdater = self as UISearchResultsUpdating
+		searchController.searchBar.placeholder = "Search"
+		searchController.searchBar.setValue("Cancel", forKey:"_cancelButtonText")
+		
+		if #available(iOS 11.0, *) {
+			searchController.obscuresBackgroundDuringPresentation = false
+			navigationItem.searchController = searchController
+		}
+	}
+	
+	func searchBarIsEmpty() -> Bool {
+	  	return searchController.searchBar.text?.isEmpty ?? true
+	}
+	
+	func isFiltering() -> Bool {
+		return searchController.isActive && !searchBarIsEmpty()
+	}
+	
+	func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+		filteredPropertyViewModels = propertyViewModels.filter({( property : PropertyViewModel) -> Bool in
+			return property.name.lowercased().contains(searchText.lowercased())
+		})
+
+		tableView.reloadData()
 	}
 
 }
@@ -47,22 +79,36 @@ class PropertiesViewController: BaseViewController {
 extension PropertiesViewController: UITableViewDelegate, UITableViewDataSource {
 	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		if isFiltering() {
+			return filteredPropertyViewModels.count
+		}
+		
 		return propertyViewModels.count
 	}
 	
 	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-		return 240
+		return 256
 	}
 	
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "PropertyTableViewCell", for: indexPath) as? PropertyTableViewCell
 		
-		cell?.propertyViewModel = propertyViewModels[indexPath.row]
+		if isFiltering() {
+			cell?.propertyViewModel = filteredPropertyViewModels[indexPath.row]
+		} else {
+			cell?.propertyViewModel = propertyViewModels[indexPath.row]
+		}
 		
 		return cell ?? UITableViewCell()
 	}
 	
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 		tableView.deselectRow(at: indexPath, animated: true)
+	}
+}
+
+extension PropertiesViewController: UISearchResultsUpdating {
+	func updateSearchResults(for searchController: UISearchController) {
+		filterContentForSearchText(searchController.searchBar.text!)
 	}
 }
