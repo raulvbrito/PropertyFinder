3766cbb6f2ec1acabbf2f114a9aa47bde4d50c6c
Raul Brito <raulvbrito@gmail.com>
2019-02-22 21:54:11 -0300

    Property Map configuration

---
+++ PropertyFinder/Views/PlaceMarkerView.swift
@@ -0,0 +1,22 @@
+//
+//  PlaceMarkerView.swift
+//  PropertyFinder
+//
+//  Created by Raul Brito on 22/02/19.
+//  Copyright © 2019 Raul Brito. All rights reserved.
+//
+
+import UIKit
+
+class PlaceMarkerView: UIView {
+
+	@IBOutlet var roundedView: UIView!
+	@IBOutlet var logoImageView: UIImageView!
+	@IBOutlet var locationPinImageView: UIImageView!
+	
+	override func draw(_ rect: CGRect) {
+		let locationPinImage = UIImage(named: "location_pin")
+		let tintedImage = locationPinImage?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
+		locationPinImageView.image = tintedImage
+	}
+}
